1390137c4c3d78de896cd052f7f3177a5a9913f7
seono9y <144989211+seono9y@users.noreply.github.com>
2023-11-09 15:30:44 +0900

    03

--- exercise_10/exercise_10/main.c
+++ exercise_10/exercise_10/main.c
@@ -6,22 +6,12 @@
 //
 
 #include <stdio.h>
+#include <string.h>
 
-#include <stdio.h>
 int main(void) {
-    int i=0;
-    char str[4];
-    str[0] = 'a';
-    str[1] = 'b';
-    str[2] = 'c';
-    str[3] = '\0';
+    char src[] = "The worst things to eat before you sleep";
+    char dst[100];
+    strcpy(dst, src);
     
-    printf("%s\n", str);
-    
-    
-    while (str[i] != '\0') {
-            printf("%c", str[i]);
-            i++;
-        
-    }
+    printf("copied string : %s", dst);
 }
