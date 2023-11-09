174fbd6772024a493ac1816605817c7ed79d17f0
seono9y <144989211+seono9y@users.noreply.github.com>
2023-11-09 15:16:37 +0900

    01

---
+++ exercise_10/exercise_10/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  exercise_10
+//
+//  Created by 추서연 on 2023/11/09.
+//
+
+#include <stdio.h>
+
+#include <stdio.h>
+int main(void){
+    char code1 = 'A';
+    int code2 = 65;
+    printf("code1 = %c, (%d)\n", code1, code1);
+printf("code2 = %c, (%d)\n", code2, code2);
+}
